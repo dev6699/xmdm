d3b23ed028d1e8319a761b195591a751b64d37f4
dev6699 <kweijack.dev@gmail.com>
2026-06-04 08:14:54 +0800

    refactor(xmdm): trim host surface

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -3,33 +3,15 @@ package main
 import (
 	"flag"
 	"log"
-	"net/http"
 
-	"xmdm/server/internal/api/v1"
-	"xmdm/server/internal/auth"
-	"xmdm/server/internal/config"
+	"xmdm/server/host"
 )
 
 func main() {
 	configPath := flag.String("config", "", "Path to YAML configuration file")
 	flag.Parse()
 
-	cfg, err := config.LoadConfig(*configPath)
-	if err != nil {
-		log.Fatalf("Failed to load configuration: %v", err)
-	}
-
-	username := cfg.Admin.Username
-	password := cfg.Admin.Password
-	sessionTTL := cfg.Server.SessionTTL
-
-	svc := auth.NewService(username, password, sessionTTL)
-	deps := v1.NewDeps(cfg)
-	mux := v1.NewMux(svc, deps)
-
-	addr := cfg.Server.Address
-	log.Printf("xmdm server listening on %s", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
+	if err := host.Run(*configPath); err != nil {
 		log.Fatal(err)
 	}
 }
--- server/host/host.go
+++ server/host/host.go
@@ -1,8 +1,8 @@
 package host
 
 import (
+	"log"
 	"net/http"
-	"time"
 
 	internalv1 "xmdm/server/internal/api/v1"
 	"xmdm/server/internal/auth"
@@ -10,64 +10,41 @@ import (
 	internalplugins "xmdm/server/internal/plugins"
 )
 
-type Config = config.Config
-type ServerConfig = config.ServerConfig
-type PostgresConfig = config.PostgresConfig
-type MQTTConfig = config.MQTTConfig
-type DeviceConfig = config.DeviceConfig
-type ObjectStoreConfig = config.ObjectStoreConfig
-type AdminConfig = config.AdminConfig
-
-type Permission = auth.Permission
-type Session = auth.Session
-type Service = auth.Service
-
-type Dependencies = internalv1.Dependencies
 type RouteSpec = internalplugins.RouteSpec
 type DeviceAction = internalplugins.DeviceAction
 type CommandType = internalplugins.CommandType
 type Plugin = internalplugins.Plugin
-type Manager = internalplugins.Manager
 
-const SessionCookieName = auth.SessionCookieName
-
-const (
-	PermissionAdminRead    = auth.PermissionAdminRead
-	PermissionAdminWrite   = auth.PermissionAdminWrite
-	PermissionDevicesRead  = auth.PermissionDevicesRead
-	PermissionDevicesWrite = auth.PermissionDevicesWrite
-)
-
-var ErrInvalidCredentials = auth.ErrInvalidCredentials
-
-func LoadConfig(path string) (*Config, error) { return config.LoadConfig(path) }
-
-func AllPermissions() []Permission { return auth.AllPermissions() }
-
-func HasPermission(perms []Permission, target Permission) bool {
-	return auth.HasPermission(perms, target)
+type HostedPlugin interface {
+	Mount(*http.ServeMux)
+	CorePlugin() Plugin
 }
 
-func NewService(username, password string, sessionTTL time.Duration) *Service {
-	return auth.NewService(username, password, sessionTTL)
-}
-
-func NewServiceWithPermissions(username, password string, sessionTTL time.Duration, permissions []Permission) *Service {
-	return auth.NewServiceWithPermissions(username, password, sessionTTL, permissions)
-}
-
-func NewDeps(cfg *Config) Dependencies { return internalv1.NewDeps(cfg) }
-
-func NewMux(svc *Service, deps Dependencies) http.Handler { return internalv1.NewMux(svc, deps) }
-
-func Disabled() *Manager              { return internalplugins.Disabled() }
-func New(defs ...Plugin) *Manager     { return internalplugins.New(defs...) }
-func Enabled(defs ...Plugin) *Manager { return internalplugins.Enabled(defs...) }
-
-func Mount(mux *http.ServeMux, prefix string, handler http.Handler) {
-	if mux == nil || handler == nil {
-		return
+func Run(configPath string, plugins ...HostedPlugin) error {
+	cfg, err := config.LoadConfig(configPath)
+	if err != nil {
+		return err
 	}
-	mux.Handle(prefix+"/", http.StripPrefix(prefix, handler))
-	mux.Handle(prefix, http.StripPrefix(prefix, handler))
+
+	deps := internalv1.NewDeps(cfg)
+	if len(plugins) > 0 {
+		defs := make([]internalplugins.Plugin, 0, len(plugins))
+		mounts := make([]func(*http.ServeMux), 0, len(plugins))
+		for _, plugin := range plugins {
+			if plugin == nil {
+				continue
+			}
+			defs = append(defs, plugin.CorePlugin())
+			mounts = append(mounts, plugin.Mount)
+		}
+		if len(defs) > 0 {
+			deps.PluginManager = internalplugins.New(defs...)
+		}
+		deps.ExtraRootMounts = append(deps.ExtraRootMounts, mounts...)
+	}
+
+	svc := auth.NewService(cfg.Admin.Username, cfg.Admin.Password, cfg.Server.SessionTTL)
+	handler := internalv1.NewMux(svc, deps)
+	log.Printf("xmdm server listening on %s", cfg.Server.Address)
+	return http.ListenAndServe(cfg.Server.Address, handler)
 }
